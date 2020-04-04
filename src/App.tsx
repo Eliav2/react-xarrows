6a7a865abcea695711b5786552bee802e05a7b39
Eliav2 <47307889+Eliav2@users.noreply.github.com>
2020-04-05 01:09:39 +0300

    Added smart curvness
    the lines now curves smoothly

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from "react";
 import { line, box, point } from "./types";
-// import { string } from "prop-types";
 import Xarrows from "./Xarrow";
 
 const titleStyle = {
@@ -27,16 +26,16 @@ const boxStyle = {
 
 const App: React.FC = () => {
   const [boxes, setBoxes] = useState<box[]>([
-    { id: "box1", x: 50, y: 30, ref: useRef(null) },
-    { id: "box2", x: 330, y: 70, ref: useRef(null) },
-    { id: "box3", x: 230, y: 115, ref: useRef(null) }
+    { id: "box1", x: 230, y: 30, ref: useRef(null) },
+    { id: "box2", x: 100, y: 200, ref: useRef(null) }
+    // { id: "box3", x: 230, y: 115, ref: useRef(null) }
   ]);
   useRef(null);
 
   const [lines, setLines] = useState<line[]>([
-    { from: "box1", to: "box2" },
-    { from: "box2", to: "box3" },
-    { from: "box3", to: "box1" }
+    { from: "box1", to: "box2" }
+    // { from: "box2", to: "box3" },
+    // { from: "box3", to: "box1" }
   ]);
 
   const [lastPoint, setLastPoint] = useState<point>({ x: 0, y: 0 });
--- src/Xarrow.tsx
+++ src/Xarrow.tsx
@@ -5,8 +5,9 @@ type anchorType = "auto" | "middle" | "left" | "right" | "top" | "bottom";
 type props = {
   start: HTMLElement;
   end: HTMLElement;
-  startAnchor?: anchorType;
-  endAnchor?: anchorType;
+  startAnchor?: anchorType | anchorType[];
+  endAnchor?: anchorType | anchorType[];
+  curveness?: number;
 };
 
 type point = { x: number; y: number };
@@ -16,7 +17,7 @@ function Xarrow(props: props) {
   // const [prevProps, setPrevProps] = useState(null);
   // const [prevPosState, setPrevPosState] = useState({ start: { x: 0, y: 0 }, end: { x: 0, y: 0 } });
   const [prevPosState, setPrevPosState] = useState(null);
-  const [hasMounted, setHasMounted] = useState(false);
+  // const [hasMounted, setHasMounted] = useState(false);
   //initial state
   const [s, setS] = useState({
     cx0: 0, //x start position of the canvas
@@ -26,7 +27,12 @@ function Xarrow(props: props) {
     x1: 0, //the x starting point of the line inside the canvas
     y1: 0, //the y starting point of the line inside the canvas
     x2: 0, //the x ending point of the line inside the canvas
-    y2: 0 //the y ending point of the line inside the canvas
+    y2: 0, //the y ending point of the line inside the canvas
+
+    cpx1: 0, // control points - control the curveness of the line
+    cpy1: 0,
+    cpx2: 0,
+    cpy2: 0
   });
 
   const [canvasStartPos, setCanvasStartPos] = useState<point>({ x: 0, y: 0 });
@@ -45,16 +51,14 @@ function Xarrow(props: props) {
     setCanvasStartPos(selfRef.current.getBoundingClientRect());
     let startingPos = getPos();
     setPrevPosState(startingPos);
-    setHasMounted(true);
-    console.log(props.start.current.id);
   }, []);
 
   useEffect(() => {
+    // triggers position update when prevPosState here handed
     if (prevPosState) updatePosition(prevPosState);
   }, [prevPosState]);
 
   useEffect(() => {
-    // console.log("useEffect");
     let posState = getPos();
     if (!lodash.isEqual(prevPosState, posState)) {
       setPrevPosState(posState);
@@ -62,6 +66,10 @@ function Xarrow(props: props) {
   });
 
   const updatePosition = positions => {
+    // Do NOT call thie function directly.
+    // you should set position by 'setPrevPosState(posState)' and that will trigger
+    // this function in the useEffect hook.
+
     let { start: s } = positions;
     let { end: e } = positions;
     let sw = s.right - s.x; //start element width
@@ -77,14 +85,14 @@ function Xarrow(props: props) {
     let dy = edy;
 
     var startAnchorOffsets = [
-      // ["middle", -sw / 2, -sh / 2],
+      ["middle", s.x + sw / 2, s.y + sh / 2],
       ["left", s.x, s.y + sh / 2],
       ["right", s.x + sw, s.y + sh / 2],
       ["top", s.x + sw / 2, s.y],
       ["bottom", s.x + sw / 2, s.y + sh]
     ];
     var endAnchorOffsets = [
-      // ["middle", ew / 2, eh / 2],
+      ["middle", e.x + ew / 2, e.y + eh / 2],
       ["left", e.x, e.y + eh / 2],
       ["right", e.x + ew, e.y + eh / 2],
       ["top", e.x + ew / 2, e.y],
@@ -94,11 +102,31 @@ function Xarrow(props: props) {
       //length of line
       return Math.sqrt((p1[1] - p2[1]) ** 2 + (p1[2] - p2[2]) ** 2);
     };
+
+    let startAnchorPossabilties = Array.isArray(props.startAnchor)
+      ? props.startAnchor
+      : [props.startAnchor];
+    let endAnchorPossabilties = Array.isArray(props.endAnchor)
+      ? props.endAnchor
+      : [props.endAnchor];
     const closestPairOfPoints = () => {
+      // closes tPair Of Points which feet to the specifed anchors
       let minDist = Infinity;
       let closestPair;
-      for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 4; j++) {
+      for (let i = 0; i < 5; i++) {
+        if (
+          !startAnchorPossabilties.includes("auto") &&
+          !startAnchorPossabilties.includes(startAnchorOffsets[i][0])
+        ) {
+          continue;
+        }
+        for (let j = 0; j < 5; j++) {
+          if (
+            !endAnchorPossabilties.includes("auto") &&
+            !endAnchorPossabilties.includes(endAnchorOffsets[j][0])
+          ) {
+            continue;
+          }
           let d = dist(startAnchorOffsets[i], endAnchorOffsets[j]);
           if (d < minDist) {
             minDist = d;
@@ -109,11 +137,9 @@ function Xarrow(props: props) {
       return closestPair;
     };
 
-    let closeset =
-      props.startAnchor === "auto" || props.endAnchor === "auto" ? closestPairOfPoints() : null;
-
-    let startAnchorType = props.startAnchor === "auto" ? closeset[0] : props.startAnchor;
-    let endAnchorType = props.endAnchor === "auto" ? closeset[1] : props.endAnchor;
+    let closeset = closestPairOfPoints();
+    let startAnchorType: anchorType = closeset[0];
+    let endAnchorType: anchorType = closeset[1];
     switch (startAnchorType) {
       case "middle":
         cx0 += sw / 2;
@@ -145,6 +171,7 @@ function Xarrow(props: props) {
         dx -= sw / 2;
         dy -= sh;
         cy0 -= dy + sh / 2 < 0 ? Math.min(sh / 2, -(dy - sh / 2)) : 0;
+
         break;
     }
     switch (endAnchorType) {
@@ -174,32 +201,110 @@ function Xarrow(props: props) {
         break;
     }
 
+    let cw = Math.abs(dx),
+      ch = Math.abs(dy);
+
+    let cu = props.curveness;
+
+    let cpx1 = 0,
+      cpy1 = 0,
+      cpx2 = 0,
+      cpy2 = 0;
+
+    const curvesPossabilties = {
+      hCurv: () => {
+        //horizinatl - from right to left or the opposite
+        cpx2 = cw * (1 - cu);
+        cpy2 = ch;
+        cpx1 = cw * cu;
+      },
+      vCurv: () => {
+        //vertical - from top to bottom or opposite
+        cpx2 = cw;
+        cpy2 = ch * (1 - cu);
+        cpy1 = ch * cu;
+      },
+      hvCurv: () => {
+        // start horizintaly then verticaly
+        // from v side to h side
+        if (dx * dy < 0) {
+          cpy2 = ch;
+          cpx2 = cw * (1 - cu);
+          cpy1 = ch * cu;
+        } else {
+          cpx1 = cw * cu;
+          cpx2 = cw;
+          cpy2 = ch * cu;
+        }
+      },
+      vhCurv: () => {
+        // start verticaly then horizintaly
+        // from h side to v side
+        if (dx * dy < 0) {
+          cpy1 = 0;
+          cpx1 = cw * cu; //blue?
+          cpy2 = ch * (1 - cu);
+          cpx2 = cw;
+        } else {
+          console.log("heyyy", cpy1);
+          cpy1 = ch * cu;
+          cpx2 = cw * (1 - cu);
+          cpy2 = ch;
+        }
+      }
+    };
+
+    const includeAll = (l: string[], findAll: string[]): boolean => {
+      let isTrue = true;
+      findAll.forEach(findMe => {
+        if (!l.includes(findMe)) isTrue = false;
+      });
+      return isTrue;
+    };
+
+    let st = startAnchorType,
+      et = endAnchorType;
+    if (["left", "right"].includes(st) && ["right", "left"].includes(et)) {
+      curvesPossabilties.hCurv();
+    } else if (["top", "bottom"].includes(st) && ["bottom", "top"].includes(et)) {
+      curvesPossabilties.vCurv();
+    } else if (["top", "bottom"].includes(st) && ["left", "right"].includes(et)) {
+      curvesPossabilties.vhCurv();
+    } else if (["left", "right"].includes(st) && ["top", "bottom"].includes(et)) {
+      curvesPossabilties.hvCurv();
+    }
+
     let x1 = 0,
       x2 = dx,
       y1 = 0,
       y2 = dy;
+
     if (dx < 0 && dy < 0) {
       x1 = -dx;
       y1 = -dy;
       x2 = 0;
       y2 = 0;
+      cpy1 = ch - cpy1;
+      cpy2 = ch - cpy2;
+      cpx1 = cw - cpx1;
+      cpx2 = cw - cpx2;
     } else {
       if (dx < 0) {
         y1 = dy;
         y2 = 0;
         x2 = -dx;
+        cpy1 = ch - cpy1;
+        cpy2 = ch - cpy2;
       }
       if (dy < 0) {
         x1 = dx;
         x2 = 0;
         y2 = -dy;
+        cpx1 = cw - cpx1;
+        cpx2 = cw - cpx2;
       }
     }
-    let cw = Math.abs(dx),
-      ch = Math.abs(dy);
-
-    // setPrevPosState(positions);
-    setS({ cx0, cy0, x1, x2, y1, y2, cw, ch });
+    setS({ cx0, cy0, x1, x2, y1, y2, cw, ch, cpx1, cpy1, cpx2, cpy2 });
   };
 
   return (
@@ -208,24 +313,29 @@ function Xarrow(props: props) {
       width={s.cw}
       height={s.ch}
       style={{
-        // border: "1px solid black",
+        border: "1px yellow dashed",
         position: "absolute",
         left: s.cx0,
         top: s.cy0,
         pointerEvents: "none"
       }}
     >
-      <line
-        x1={s.x1}
-        x2={s.x2}
-        y1={s.y1}
-        y2={s.y2}
-        style={{ stroke: "rgb(255,0,0)", strokeWidth: "2" }}
+      <circle r="5" cx={s.cpx1} cy={s.cpy1} fill="green" />
+      <circle r="5" cx={s.cpx2} cy={s.cpy2} fill="blue" />
+      <path
+        d={`M ${s.x1} ${s.y1} C  ${s.cpx1} ${s.cpy1}, ${s.cpx2} ${s.cpy2}, ${s.x2} ${s.y2}`}
+        stroke="red"
+        strokeWidth="3"
+        fill="transparent"
       />
     </svg>
   );
 }
 
-Xarrow.defaultProps = { startAnchor: "auto", endAnchor: "auto" };
+Xarrow.defaultProps = {
+  startAnchor: "auto",
+  endAnchor: "auto",
+  curveness: 0.7
+};
 
 export default Xarrow;
