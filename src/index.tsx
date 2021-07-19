62a3bee807345b54c3cf97e8ff54bf35dea07b04
eliav2 <louski.a@gmail.com>
2021-07-19 22:31:08 +0300

    Xelem compoenent removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 export { default } from './Xarrow/Xarrow';
 export * from './types';
 export * from './constants';
-export { default as Xelem } from './Xelem';
 export { default as Xwrapper } from './Xwrapper';
 export { default as useXarrow } from './useXarrow';
