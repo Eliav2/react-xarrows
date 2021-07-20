6abd5a4dfc06c231813a1ad6564817776a4ad55e
eliav2 <louski.a@gmail.com>
2021-07-20 12:35:57 +0300

    example 4 update v2

--- examples/src/examplesFiles/Example4.jsx
+++ examples/src/examplesFiles/Example4.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
 import Xarrow, { useXarrow, Xwrapper } from 'react-xarrows';
 import Draggable from 'react-draggable';
 
