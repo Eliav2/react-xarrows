de62da155f6484db6abaefa0c54a4f63a7a7cf07
eliav2 <louski.a@gmail.com>
2021-08-16 18:44:15 +0300

    .

--- src/useXarrow.tsx
+++ src/useXarrow.tsx
@@ -2,18 +2,31 @@ import React, { useContext, useLayoutEffect, useState } from 'react';
 import { XelemContext } from './Xwrapper';
 
 const useXarrow = () => {
+  const [mount, setMount] = useState(false);
   const [, setRender] = useState({});
   const reRender = () => setRender({});
 
   let updateXarrow = useContext(XelemContext);
-  if (!updateXarrow) updateXarrow = () => {};
-  // throw new Error(
-  //   "'Xwrapper' is required around element using 'useXarrow' hook! wrap your xarrows and connected elements with Xwrapper! "
-  // );
+  if (!updateXarrow) {
+    if (mount)
+      updateXarrow = () => {
+        console.warn(
+          "react-xarrow: can't find Xwrapper around connected element instance!\n" +
+            'useXarrow hook would not trigger update on Xarrow instances!\n' +
+            'wrap connected elements with Xwrapper.'
+        );
+      };
+    else updateXarrow = () => {};
+  }
 
   useLayoutEffect(() => {
     updateXarrow();
   });
+  useLayoutEffect(() => {
+    // console.log('mount');
+    setMount(true);
+  }, []);
+
   return reRender;
 };
 
