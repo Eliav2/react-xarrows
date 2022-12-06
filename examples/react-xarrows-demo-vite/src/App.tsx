d15b8b49027541a747100d011e8b70c34760000d
eliav2 <louski.a@gmail.com>
2022-12-06 02:02:27 +0200

    .

--- examples/react-xarrows-demo-vite/src/App.tsx
+++ examples/react-xarrows-demo-vite/src/App.tsx
@@ -1,45 +1,87 @@
-import './App.css';
-import { Xline, Xarrow } from 'react-xarrows/src/redesign/mock';
-import React from 'react';
+import "./App.css";
+import { XLine, XArrow, XArrowElement, XWrapper, useXArrow } from "react-xarrows/src/redesign/mock";
+import React, { useRef } from "react";
+import Draggable from "react-draggable";
+import useRerender from "shared/hooks/useRerender";
 
 interface BoxProps extends React.HTMLProps<HTMLDivElement> {
   children?: React.ReactNode;
 }
 
 const Box = ({ children, style, ...props }: BoxProps) => {
+  const render = useRerender();
+  console.log(children, "render!");
+
+  const renderXArrow = useXArrow();
+  // console.log(renderXArrow);
+  const ref = useRef<HTMLDivElement>(null);
   return (
-    <div
-      style={{
-        border: '1px solid',
-        borderRadius: 8,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 80,
-        ...style,
+    <Draggable
+      nodeRef={ref}
+      grid={[10, 10]}
+      onDrag={() => {
+        console.log(children, "onDrag!");
+        // console.log(renderXArrow);
+        renderXArrow.render();
+        // renderXArrow;
       }}
-      {...props}>
-      {children}
+    >
+      <div
+        ref={ref}
+        style={{
+          border: "1px solid",
+          borderRadius: 8,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: 80,
+          padding: 30,
+          ...style,
+        }}
+        {...props}
+      >
+        <button onClick={render}>render</button>
+        {children}
+      </div>
+    </Draggable>
+  );
+};
+
+const MyArrows = () => {
+  console.log("MyArrows render");
+
+  return (
+    <XArrow start={"box1"} end={"box2"}>
+      <XLine />
+      {/*<XArrowElement />*/}
+      {/*<Xline x={'0%'} y={'0%'}/>*/}
+      {/*<path d="M10 10" />*/}
+      {/*<path d="M 10 10 H 90 V 90 H 10 Z" fill="transparent" stroke="black" />*/}
+    </XArrow>
+  );
+};
+
+const MyBoxes = () => {
+  console.log("MyBoxes render");
+  return (
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
+      <Box id={"box1"}>box1</Box>
+      <Box id={"box2"}>box2</Box>
     </div>
   );
 };
 
 function App() {
   return (
-    <div className="App">
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <Box id={'box1'}>box1</Box>
-        <Box id={'box2'}>box2</Box>
+    <React.StrictMode>
+      <div className="App">
+        <XWrapper>
+          <MyBoxes />
+          <div style={{ height: 50 }} />
+          <MyArrows />
+        </XWrapper>
       </div>
-      <Xarrow start={'box1'} end={'box2'}>
-        <Xline />
-        {/*<Xline x={'0%'} y={'0%'}/>*/}
-        {/*<path d="M10 10" />*/}
-        {/*<path d="M 10 10 H 90 V 90 H 10 Z" fill="transparent" stroke="black" />*/}
-      </Xarrow>
-
-
-    </div>
+    </React.StrictMode>
   );
 }
 
--- src/redesign/mock.tsx
+++ src/redesign/mock.tsx
@@ -1,8 +1,12 @@
-import { Contains } from '../privateTypes';
-import React, { useRef } from 'react';
-import usePosition from "shared/hooks/usePosition"
+import { Contains } from "../privateTypes";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
+import usePosition, { positionType } from "shared/hooks/usePosition";
+import useRerender from "shared/hooks/useRerender";
+import { pick } from "shared/utils";
+// import { useElement } from "../hooks/useElement";
 
-type XElemRefType = React.MutableRefObject<any> | string | Contains<{ x: number; y: number }>;
+type Point = Contains<{ x: number; y: number }>;
+type XElemRefType = React.MutableRefObject<any> | string | Point;
 
 interface XarrowProps {
   children: React.ReactNode;
@@ -14,32 +18,118 @@ interface XarrowProps {
   svgCanvasProps?: React.SVGProps<SVGSVGElement>;
 }
 
-export const Xarrow = (props: XarrowProps) => {
+export const XArrow = (props: XarrowProps) => {
+  const render = useRerender();
   const rootDivRef = useRef<HTMLDivElement>(null);
   const svgCanvasRef = useRef<SVGSVGElement>(null);
+
+  const rootElem = usePosition(rootDivRef.current);
+  const startElem = usePosition(getElementByPropGiven(props.start));
+  const endElem = usePosition(getElementByPropGiven(props.end));
+
+  // let startPoint = { x: (startElem?.left ?? 0) - (rootElem?.left ?? 0) };
+  let startPoint = { x: 0, y: 0 };
+  let endPoint = { x: 0, y: 0 };
+  if (rootElem) {
+    if (startElem)
+      startPoint = {
+        x: startElem.left - rootElem.left + startElem.width / 2,
+        y: startElem.top - rootElem.top + startElem.height / 2,
+      };
+    if (endElem)
+      endPoint = {
+        x: endElem.left - rootElem.left + endElem.width / 2,
+        y: endElem.top - rootElem.top + endElem.height / 2,
+      };
+  }
+  const contextValue = { startElem, endElem, startPoint, endPoint };
+
+  const xWrapperContextVal = useXWrapperContext();
+  useLayoutEffect(() => {
+    xWrapperContextVal.render = render;
+  }, [rootDivRef.current]);
+
   return (
     <div
       ref={rootDivRef}
-      style={{ position: 'absolute', pointerEvents: 'none', ...props.divWrapperProps?.style }}
-      {...props.divWrapperProps}>
+      style={{ position: "absolute", pointerEvents: "none", ...props.divWrapperProps?.style }}
+      {...props.divWrapperProps}
+    >
       <svg
         style={{
-          position: 'absolute',
-          fill: 'transparent',
-          overflow: 'visible',
+          position: "absolute",
+          fill: "transparent",
+          overflow: "visible",
           ...props.svgCanvasProps?.style,
         }}
         ref={svgCanvasRef}
-        {...props.svgCanvasProps}>
-        {props.children}
-
+        {...props.svgCanvasProps}
+      >
+        <XArrowContext.Provider value={contextValue}>{props.children}</XArrowContext.Provider>
       </svg>
     </div>
   );
 };
 
-interface XlineProps {}
+const XArrowContext = React.createContext<{ startElem: positionType; endElem: positionType; startPoint: Point; endPoint: Point }>({
+  startElem: null,
+  endElem: null,
+  startPoint: { x: 0, y: 0 },
+  endPoint: { x: 0, y: 0 },
+});
+export const useXArrowContext = () => React.useContext(XArrowContext);
 
-export const Xline = (props: XlineProps) => {
-  return <div>Xline</div>;
+interface XWrapperProps {
+  children: React.ReactNode;
+}
+
+const XWrapperContextDefault = {
+  render: () => {},
+  set: false,
+};
+export const XWrapper = (props: XWrapperProps) => {
+  // value of the context will be mutated after mount of first XArrow
+  return <XWrapperContext.Provider value={XWrapperContextDefault}>{props.children}</XWrapperContext.Provider>;
+};
+const XWrapperContext = React.createContext<{ render: () => void; set: boolean }>(XWrapperContextDefault);
+const useXWrapperContext = () => React.useContext(XWrapperContext);
+export const useXArrow = (): { render: () => void } => useXWrapperContext();
+
+interface XArrowElementProps {}
+
+export const XArrowElement = (props: XArrowElementProps) => {
+  const val = useXArrowContext();
+  console.log(val);
+  return (
+    <g style={{ transform: "translate(0)" }}>
+      <path d="M 10 10 H 90 V 90 H 10 Z" fill="transparent" stroke="black" />
+    </g>
+  );
+};
+
+interface XLineProps {}
+
+export const XLine = (props: XLineProps) => {
+  const val = useXArrowContext();
+  // console.log(val);
+  // return <path d="M 10 10 H 90 V 90 H 10 Z" fill="transparent" stroke="black" />;
+  return (
+    <line
+      x1={val.startPoint.x}
+      y1={val.startPoint.y}
+      x2={val.endPoint.x}
+      y2={val.endPoint.y}
+      fill="transparent"
+      stroke="white"
+      strokeWidth={3}
+    />
+  );
+};
+
+export const getElementByPropGiven = (ref: XElemRefType): HTMLElement => {
+  let myRef;
+  if (typeof ref === "string") {
+    myRef = document.getElementById(ref);
+  } else myRef = ref?.current;
+  return myRef;
 };
